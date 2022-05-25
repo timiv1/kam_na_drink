bdcb348863cca16fcc638d6df3303695297652dd
timi vovk <timi.vovk@gmail.com>
2022-05-25 12:27:20 +0200

    FE - Addded base page component.

---
+++ frontend/src/components/BasePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>{{ title }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">{{ title }}</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <slot></slot>
+    </ion-content>
+  </ion-page>
+</template>
+<script lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "BasePage",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+  },
+  props: {
+    title: { required: true, type: String },
+  },
+});
+</script>
